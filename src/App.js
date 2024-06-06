0bbe8ef672e2118ec61aae7427cb241fb52b3bea
Meet <codewithmeet25@gmail.com>
2024-06-06 17:23:56 +0530

    sign in and signup frorm && login/header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <div className="text-3xl font-bold text-emerald-800">
-      Lets build Netflix chat-gpt
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
