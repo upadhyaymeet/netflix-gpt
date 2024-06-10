75f5d3110b8f036c76c440dfffdecf1053305367
Meet <codewithmeet25@gmail.com>
2024-06-10 17:01:00 +0530

    custom hook for api call and store data

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,23 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Header from "./Header";
-import { API_OPTIONS } from "../utils/constants";
-import { useDispatch } from "react-redux";
-import { addNowPlayingMovies } from "../utils/movieSlice";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 
 const Browse = () => {
+  useNowPlayingMovies();
 
-  const dispatch = useDispatch()
-
-  const getNowPlayingMovies = async () => {
-    const data = await fetch("https://api.themoviedb.org/3/movie/now_playing?page=1", API_OPTIONS);
-    const json = await data.json();
-    console.log(json.results);
-    dispatch(addNowPlayingMovies(json.results))
-  };
-
-  useEffect(()=>{
-    getNowPlayingMovies()
-  },[]);
   return (
     <div>
       <Header />
