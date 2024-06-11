c0fe446a868e1b36095f7d5a855ab66f5e87644e
Meet <codewithmeet25@gmail.com>
2024-06-11 15:13:16 +0530

    fetch the data for video trailer and store in redux store and embeded code from youtube

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailerVideo } from "../utils/movieSlice";
 
-const VideoBackground = () => {
+const VideoBackground = ({ movieId }) => {
+  const trailerVideo = useSelector(store=>store.movies?.trailerVideo)
+  const disptach = useDispatch();
+  // console.log(trailerVideo)
+
+  //fetch trailer
+  const getMovieVideos = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/" +
+        movieId +
+        "/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    // console.log(json)
+
+    const filterData = json.results.filter((video) => video.type === "Trailer");
+    // console.log(filterData);
+    const trailer = filterData.length ? filterData[0] : json.results[0];
+    // console.log(trailer);
+    disptach(addTrailerVideo(trailer))
+  };
+
+  useEffect(() => {
+    getMovieVideos();
+  }, []);
   return (
-    <div>VideoBackground</div>
-  )
-}
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src={"https://www.youtube.com/embed/"+trailerVideo?.key+"?si=WUojTXOnD6kmjwgt"}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+      ></iframe>
+    </div>
+  );
+};
 
-export default VideoBackground
+export default VideoBackground;
