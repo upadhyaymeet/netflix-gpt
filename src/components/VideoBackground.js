7ca91a96bf41f86daab0cddd399a0091fa71947e
Meet <codewithmeet25@gmail.com>
2024-06-12 11:57:13 +0530

    video autoplay working and complete video background and videoTitle

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,18 +1,22 @@
-import React, {} from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import useMovieTrailer from "../hooks/useMovieTrailer";
 
-
 const VideoBackground = ({ movieId }) => {
-  useMovieTrailer(movieId)
-  const trailerVideo = useSelector(store=>store.movies?.trailerVideo)
-  
+  useMovieTrailer(movieId);
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+
   return (
-    <div>
+    <div className="w-screen">
       <iframe
-        width="560"
-        height="315"
-        src={"https://www.youtube.com/embed/"+trailerVideo?.key+"?si=WUojTXOnD6kmjwgt"}
+        className="w-screen aspect-video"
+        src={
+          "https://www.youtube.com/embed/" +
+          trailerVideo?.key +
+          "?autoplay=0&mute=1" +
+          "?si=WUojTXOnD6kmjwgt"
+          
+        }
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36 px-12">
+    <div className="w-screen aspect-video pt-[15%] px-24 absolute text-white bg-gradient-to-r from-black">
       <h1 className="text-6xl font-bold w-1/2">{title}</h1>
-      <p className="py-6 text-lg w-1/4">{overview}</p>
+      <p className="py-6 text-lg w-1/2">{overview}</p>
       <div>
-        <button className="bg-gray-500 text-white p-4 px-12 text-lg bg-opacity-50 rounded-lg">
+        <button className="bg-white text-black p-4 px-12 text-lg rounded-lg hover:bg-opacity-30">
           ▶ Play
         </button>
         <button className="mx-2 bg-gray-500 text-white p-4 px-12 text-lg bg-opacity-50 rounded-lg">
