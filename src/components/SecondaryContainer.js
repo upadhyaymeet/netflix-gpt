861831a2aade58421021f6bfaacff76d2b1b422c
Meet <codewithmeet25@gmail.com>
2024-06-12 13:34:41 +0530

    fetch popular movies integrated and send to store and display on page

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -9,7 +9,7 @@ const SecondaryContainer = () => {
     <div className="bg-black">
       <div className="-mt-40 pl-12 relative z-20">
         <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-        <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+        <MovieList title={"Popular"} movies={movies.popularMovies} />
         <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
         <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
         <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
