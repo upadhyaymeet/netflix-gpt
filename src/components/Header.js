a47efe23ef2f70490f8a595ba6dd0003aa1005ff
Meet <codewithmeet25@gmail.com>
2024-06-12 17:38:57 +0530

    fix bug when gpt search click then lang option show and few changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,6 +12,7 @@ import { changeLanguage } from "../utils/configSlice";
 const Header = () => {
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
   const dispatch = useDispatch();
 
   const handleSignOut = () => {
@@ -50,27 +51,34 @@ const Header = () => {
     dispatch(toggleGptSearchView());
   };
 
-  const handleLanguageChange = (e) =>{
+  const handleLanguageChange = (e) => {
     // console.log(e.target.value)
-    dispatch(changeLanguage(e.target.value))
-  }
+    dispatch(changeLanguage(e.target.value));
+  };
 
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img className="w-44" src={LOGO} alt="netlfix-logo" />
       {user && (
         <div className="flex p-2">
-          <select className="p-2 m-2 bg-gray-900 text-white" onChange={handleLanguageChange}>
-            {SUPPORTED_LANGUAGES.map((lang)=>(
-               <option key={lang.identifier} value={lang.identifier}>{lang.name}</option>
-            ))}
-           
-          </select>
+          {showGptSearch && (
+            <select
+              className="p-2 m-2 bg-gray-900 text-white"
+              onChange={handleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
+
           <button
             className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
             onClick={handleGptSearch}
           >
-            GPT Search
+            {showGptSearch ? "Home" : "GPT Search"}
           </button>
           <img className="w-12 h-12" alt="user-icon" src={user?.photoURL} />
           <button className="font-bold text-white" onClick={handleSignOut}>
