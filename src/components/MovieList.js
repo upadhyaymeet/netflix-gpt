cbf08b99a8efa992e2fcd2a6ec230ce60581b277
Meet <codewithmeet25@gmail.com>
2024-06-12 13:24:21 +0530

    overlap container

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,9 +2,9 @@ import React from "react";
 import MovieCard from "./MovieCard";
 
 const MovieList = ({ title, movies }) => {
-  console.log(movies);
+//   console.log(movies);
   return (
-    <div className="px-6 bg-black">
+    <div className="px-6">
       <h1 className="text-4xl py-4 text-white">{title}</h1>
       <div className="flex overflow-x-scroll">
         <div className="flex">
--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import MovieList from './MovieList'
-import { useSelector } from 'react-redux'
+import React from "react";
+import MovieList from "./MovieList";
+import { useSelector } from "react-redux";
 
 const SecondaryContainer = () => {
-  const movies = useSelector((store)=>store.movies);
+  const movies = useSelector((store) => store.movies);
 
   return (
-    <div>
-      <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
+    <div className="bg-black">
+      <div className="-mt-40 pl-12 relative z-20">
+        <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+        <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+        <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+        <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+        <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SecondaryContainer
+export default SecondaryContainer;
