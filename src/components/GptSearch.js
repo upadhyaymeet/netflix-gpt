7459ed8231eafe4246e7b22620a709cc898692b3
Meet <codewithmeet25@gmail.com>
2024-06-12 16:42:53 +0530

    Gpt search bar create

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import GptMovieSuggestion from './GptMovieSuggestion'
+import GptSearchBar from './GptSearchBar'
+import { BG_URL } from '../utils/constants'
 
 const GptSearch = () => {
   return (
-    <div>GptSearch</div>
+    <div>
+        <div className="absolute -z-10">
+        <img
+          src={BG_URL}
+          alt="bg-img"
+        />
+      </div>
+        <GptSearchBar />
+        <GptMovieSuggestion />1
+    </div>
   )
 }
 
