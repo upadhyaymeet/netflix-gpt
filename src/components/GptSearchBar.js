08d31b75ce68381610870ff1534d64c3a59bfa57
Meet <codewithmeet25@gmail.com>
2024-06-13 13:26:32 +0530

    api call to chatgpt and tmdb movie and get the results

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -2,19 +2,54 @@ import React, { useRef } from "react";
 import lang from "../utils/languageConstants";
 import { useSelector } from "react-redux";
 import openai from "../utils/openai";
+import { API_OPTIONS } from "../utils/constants";
 
 const GptSearchBar = () => {
   const langKey = useSelector((store) => store.config.lang);
   const searchText = useRef(null);
+
+  // searchMovie in tmdb
+  const searchMovieTMDB = async (movie) => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/search/movie?query=" +
+        movie +
+        "&include_adult=false&language=en-US&page=1",
+      API_OPTIONS
+    );
+
+    const json = await data.json();
+    return json.results;
+  };
+
   const handleGptSearchClick = async () => {
-    console.log(searchText.current.value);
+    // console.log(searchText.current.value);
+
     //MAKE AN API CALL TO GET MOVIE RESULT
+
+    const gptQuery =
+      "Act as a movie recommendation system and suggest some movies for the query : " +
+      searchText.current.value +
+      ". Only give me names of 5 movies, comma seperated. like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmal, Koi Mil Gaya";
+
     const gptResults = await openai.chat.completions.create({
-      messages: [{ role: "user", content: "Say this is a test" }],
+      messages: [{ role: "user", content: gptQuery }],
       model: "gpt-3.5-turbo",
     });
 
-    console.log(gptResults.choices);
+    // if(!gptResults){
+    //   console.log(searchText.current.value)
+    // }
+
+    // console.log(gptResults.choices);
+    const gptMovies = gptResults.choices?.[0]?.message?.content.split(",");
+    // console.log(gptMovies)
+    // for each movie i will search TMDB APi
+
+    const promiseArray = gptMovies.map((movie) => searchMovieTMDB(movie));
+    //[Promise, Promise, Promise, Promise]
+
+    const tmdbResults = await Promise.all(promiseArray)
+    console.log(tmdbResults)
   };
 
   return (
