b7d4e110ee2d8fe2c023a42f44f2145042d83c2f
Meet <codewithmeet25@gmail.com>
2024-06-13 16:57:35 +0530

    responsive design added

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -5,16 +5,20 @@ import { BG_URL } from '../utils/constants'
 
 const GptSearch = () => {
   return (
-    <div>
-        <div className="-z-10 fixed">
+    <>
+      <div className="-z-10 fixed">
         <img
+        className='h-screen object-cover'
           src={BG_URL}
           alt="bg-img"
         />
       </div>
+      <div className=''>
         <GptSearchBar />
         <GptMovieSuggestion />1
     </div>
+    </>
+    
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -57,8 +57,8 @@ const Header = () => {
   };
 
   return (
-    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
-      <img className="w-44" src={LOGO} alt="netlfix-logo" />
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
+      <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="netlfix-logo" />
       {user && (
         <div className="flex p-2">
           {showGptSearch && (
